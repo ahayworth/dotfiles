8aeb494ce5d86745d4d7b752b0fe70c35610faa9
Andrew Hayworth <ahayworth@gmail.com>
2019-07-20 00:11:54 -0500

    maybe syntax fix

--- dotfiles/terminal/blink.sh.js
+++ dotfiles/terminal/blink.sh.js
@@ -1,22 +1,4 @@
-black   = "#1d2021";
-red     = "#fb4934";
-green   = "#b8bb26";
-yellow  = "#fabd2f";
-blue    = "#83a598";
-magenta = "#d3869b";
-cyan    = "#8ec07c";
-white   = "#d5c4a1";
-brightBlack   = "#665c54";
-brightRed     = "#fb4934";
-brightGreen   = "#b8bb26";
-brightYellow  = "#fabd2f";
-brightBlue    = "#83a598";
-brightMagenta = "#d3869b";
-brightCyan    = "#8ec07c";
-brightWhite   = "#fbf1c7";
-
-t.prefs_.set("color-palette-overrides", [black, red, green, yellow, blue, magenta, cyan, white, brightBlack, brightRed, brightGreen, brightYellow, brightBlue, brightMagenta, brightCyan, brightWhite]);
-
+t.prefs_.set("color-palette-overrides", ["#1d2021", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#d5c4a1", "#665c54", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#fbf1c7"]);
 t.prefs_.set("foreground-color", "#d5c4a1");
 t.prefs_.set("background-color", "#1d2021");
 t.prefs_.set('cursor-color", "rgba(48,255,36,0.5)");
