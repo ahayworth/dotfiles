03f703fd9862a65f9a8e2dcb73c635e740a68038
Andrew Hayworth <ahayworth@gmail.com>
2019-07-20 00:25:59 -0500

    apparently there is a new syntax?

--- dotfiles/terminal/blink.js
+++ dotfiles/terminal/blink.js
@@ -1,4 +1,4 @@
-t.prefs_.set("color-palette-overrides", ["#1d2021", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#d5c4a1", "#665c54", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#fbf1c7"]);
-t.prefs_.set("foreground-color", "#d5c4a1");
-t.prefs_.set("background-color", "#1d2021");
-t.prefs_.set('cursor-color", "rgba(48,255,36,0.5)");
+term_set("color-palette-overrides", ["#1d2021", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#d5c4a1", "#665c54", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#fbf1c7"]);
+term_set("foreground-color", "#d5c4a1");
+term_set("background-color", "#1d2021");
+term_set('cursor-color", "rgba(48,255,36,0.5)");
