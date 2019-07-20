236fc4d92a9940a4cf86e386c559ffb9011fe506
Andrew Hayworth <ahayworth@gmail.com>
2019-07-20 00:39:45 -0500

    try another theme?

--- dotfiles/terminal/blink.js
+++ dotfiles/terminal/blink.js
@@ -1,4 +1,9 @@
-term_set("color-palette-overrides", ["#1d2021", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#d5c4a1", "#665c54", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#fbf1c7"]);
-term_set("foreground-color", "#d5c4a1");
-term_set("background-color", "#1d2021");
-term_set('cursor-color", "rgba(48,255,36,0.5)");
+var theme = {
+  background: "#1d2021",
+  foreground: "#d5c4a1",
+  color: ["#1d2021","#fb4934","#b8bb26","#fabd2f","#83a598","#d3869b","#8ec07c","#d5c4a1","#665c54","#fb4934","#b8bb26","#fabd2f","#83a598","#d3869b","#8ec07c","#fbf1c7"],
+};
+
+term_applySexyTheme(theme);
+term_set("cursor-color", "rgba(213,196,161,0.5)");
+term_set("cursor-blink", false);
