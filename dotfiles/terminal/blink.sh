f3e534ac42813e0ab52d7be2c63c81ecf3f6d241
Andrew Hayworth <ahayworth@gmail.com>
2019-07-19 23:55:12 -0500

    add blink theme

---
+++ dotfiles/terminal/blink.sh
@@ -0,0 +1,21 @@
+black   = '#1d2021';
+red     = '#fb4934';
+green   = '#b8bb26';
+yellow  = '#fabd2f';
+blue    = '#83a598';
+magenta = '#d3869b';
+cyan    = '#8ec07c';
+white   = '#d5c4a1';
+brightBlack   = '#665c54';
+brightRed     = '#fb4934';
+brightGreen   = '#b8bb26';
+brightYellow  = '#fabd2f';
+brightBlue    = '#83a598';
+brightMagenta = '#d3869b';
+brightCyan    = '#8ec07c';
+brightWhite   = '#fbf1c7';
+
+t.prefs_.set('color-palette-overrides', [black, red, green, yellow, blue, magenta, cyan, white, brightBlack, brightRed, brightGreen, brightYellow, brightBlue, brightMagenta, brightCyan, brightWhite];
+
+t.prefs_.set('foreground-color', '#d5c4a1');
+t.prefs_.set('background-color', '#1d2021');
