9f947485683a7cb31187b3ae905b51826c28d8f4
Andrew Hayworth <ahayworth@gmail.com>
2021-08-11 11:14:19 -0500

    Update dotfiles, remove old work configs

--- dotfiles/terminal/blink.js
+++
@@ -1,9 +0,0 @@
-var theme = {
-  background: "#1d2021",
-  foreground: "#d5c4a1",
-  color: ["#1d2021","#fb4934","#b8bb26","#fabd2f","#83a598","#d3869b","#8ec07c","#d5c4a1","#665c54","#fb4934","#b8bb26","#fabd2f","#83a598","#d3869b","#8ec07c","#fbf1c7"],
-};
-
-term_applySexyTheme(theme);
-term_set("cursor-color", "rgba(213,196,161,0.5)");
-term_set("cursor-blink", false);
